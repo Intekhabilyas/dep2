5657050ecd7d63a83a6e263e3f39fdd3431dc503
Intekhabilyas <intekhabilyas001@gmail.com>
2023-11-10 08:20:28 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app=express();
+const bodyParser=require("body-parser");
+const path = require("path");
+const PORT=3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.send("hello everyone")
+});
+
+app.listen(PORT,()=>{
+    console.log(`listening to port https://localhost:${PORT}`)
+});
