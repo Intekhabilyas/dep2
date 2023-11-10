07600888f62b6cf897d64ffc34cd3bd4b33cbd83
Intekhabilyas <intekhabilyas001@gmail.com>
2023-11-10 09:50:35 +0530

    change

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-    res.send("hello ")
+    res.send("hello everyone")
 });
 
 app.listen(PORT,()=>{
