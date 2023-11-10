af884c3c29f5ae2b2afdc92c0fd941cf9e7a3182
Intekhab Ilyas <79245783+Intekhabilyas@users.noreply.github.com>
2023-11-10 18:25:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 const bodyParser=require("body-parser");
 const path = require("path");
-const PORT=3000;
+const PORT=3400;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
